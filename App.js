9c52692ae37dd6dd9e779bc4a8fbee8d7122dc42
Nischal <dhungeln12@gmail.com>
2024-02-07 15:50:24 +0545

    learned about local image,source image and imagebackground property

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 const logoImage = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View>
-      <View style={{ flex: 1, backgroundColor: "plum" }}>
-        <Image source={logoImage} />
-      </View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Image source={logoImage} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/seed/picsum/300/300" }}
+        style={{ width: 300, height: 300 }}
+      /> */}
+
+      <ImageBackground source={logoImage} style={{ flex: 1 }}>
+        <Text> Image Text</Text>
+      </ImageBackground>
     </View>
   );
 }
